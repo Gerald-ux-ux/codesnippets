83af65e7e1ee91c20804216ece4298f3e3998d0c
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-05-10 16:26:41 +0300

    feat: stting up fonts, colors and global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// Global font
+const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetBrainsMono.className}>{children}</body>
     </html>
   );
 }
---
+++ lib/fonts.ts
@@ -0,0 +1,7 @@
+// Secondary font
+import { Fira_Mono } from "next/font/google";
+
+export const firaMono = Fira_Mono({
+  subsets: ["latin"],
+  weight: "700",
+});
