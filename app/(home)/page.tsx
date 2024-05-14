703a1bdb6ca4901f648dffa4a433e9878be6864c
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-05-14 23:37:02 +0300

    fix: ui errors and acerternity config

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,17 @@
 import NavBar from "./components/nav/nav-bar";
 import HeroSection from "./components/ui/hero-section";
+import LanguageCarousel from "./components/ui/language-carousel";
+import ProductDemo from "./components/ui/product-demo";
 import Usp from "./components/ui/usp";
 
 export default function Home() {
   return (
     <main className="flex animate-in flex-col items-center gap-12 w-full px-4 mx-auto max-w-7xl">
       <NavBar />
-
       <HeroSection />
       <Usp />
+      <ProductDemo />
+      <LanguageCarousel />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,10 @@
-import type { Config } from "tailwindcss";
+const svgToDataUri = require("mini-svg-data-uri");
+
+// bg-no-repeat bg-gradient-to-br  from-[#090a0b] via-[#15171a] to-[#16181a]
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ["class"],
@@ -10,6 +16,11 @@ const config = {
   ],
   theme: {
     extend: {
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
       textColor: {
         primary: "#F8F8F2",
         secondary: "#6E6E70",
@@ -26,33 +37,56 @@ const config = {
         primary: "#363739",
         gradient: "linear-gradient(to bottom right, #6d6d6d, #191A1C)",
       },
-      keyframes: {
-        in: {
-          "0%": { transform: "translateY(18px)", opacity: "0" },
-          "100%": { transform: "translateY(0)", opacity: "1" },
-        },
-        "in-reverse": {
-          "0%": { transform: "translateY(-18px)", opacity: "0" },
-          "100%": { transform: "translateY(0px)", opacity: "1" },
-        },
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
       animation: {
-        in: "in .6s both",
-        "in-reverse": "in-reverse .6s both",
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config;
