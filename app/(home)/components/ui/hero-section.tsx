1de488dc562cf434ea767b8edfcebc2052591137
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-05-14 12:44:29 +0300

    style: changing the hero section

--- app/(home)/components/ui/hero-section.tsx
+++ app/(home)/components/ui/hero-section.tsx
@@ -15,41 +15,21 @@ function HeroSection(props: Props) {
   return (
     <div className="flex items-center justify-center w-full rounded-2xl">
       <TextRevealCard
-        className="w-full flex items-center justify-center flex-col bg-primary "
-        text="You know the business"
-        revealText="I know the chemistry "
+        className="w-full flex items-center gap-6 justify-center flex-col bg-primary "
+        text="Boost your productivity"
+        revealText="With Code snippets "
       >
-        <TextRevealCardTitle className="text-primary">
-          Sometimes, you just need to see it.
+        <TextRevealCardTitle className="text-primary mb-4">
+          <Btn action={() => {}} name="Get Started" />
         </TextRevealCardTitle>
-        <TextRevealCardDescription className="text-secondary" >
-          This is a text reveal card. Hover over the card to reveal the hidden
-          text.
+        <TextRevealCardDescription className="text-secondary text-center">
+          Discover, share and manage code snippets across different languages
+          and frameworks. <br /> Streamline your development workflow and just
+          snippit
         </TextRevealCardDescription>
       </TextRevealCard>
     </div>
   );
-  //   <div className="border border-primary flex items-center justify-center flex-col rounded-lg p-4 w-full">
-  //     <div className=" my-24 items-center  flex flex-col gap-12 w-full">
-  //       <TextRevealCard
-  //         className='bg-none '
-  //         text="Code Snippets"
-  //         revealText="Boost Your Productivity "
-  //       >
-
-  //       </TextRevealCard>
-  //       <h1 className={cn(firaMono.className, "text-4xl text-center")}></h1>
-
-  //       <p className=" text-secondary text-center">
-  //         Discover, share and manage code snippets across <br /> different
-  //         languages and frameworks. <br /> Streamline your development workflow
-  //         and just snippit
-  //       </p>
-
-  //       <Btn action={() => {}} name="Get Started" />
-  //     </div>
-  //   </div>
-  // );
 }
 
 export default HeroSection;
