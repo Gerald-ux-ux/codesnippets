a0be3914389d04ce1c42ae4b5acd389c2b5ae537
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-05-18 15:03:22 +0300

    fix: deployment error

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,8 +1,10 @@
-// import { Webhook } from "svix";
-// import { headers } from "next/headers";
-// import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
-// import {} from "@clerk/nextjs";
-// import axios from "axios";
-// import { NextResponse } from "next/server";
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import {} from "@clerk/nextjs";
+import axios from "axios";
+import { NextResponse } from "next/server";
 
-// export async function POST(req: Request, res: Response) {}
+export async function POST(req: Request, res: Response) {
+    
+}
