69675f3c686055172b51439f9226bd5abfbd349f
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-05-23 17:54:57 +0300

    refactor: creation of a user

--- lib/backend/db/cs.ts
+++ lib/backend/db/cs.ts
@@ -33,36 +33,3 @@ if (process.env.NODE_ENV === "development") {
 
 export default clientPromise;
 
-// const MONGODB_URL = process.env.MONGO_URL!;
-
-// interface MongooseConnection {
-//   conn: Mongoose | null;
-//   promise: Promise<Mongoose> | null;
-// }
-
-// let cached: MongooseConnection = (global as any).mongoose;
-
-// if (!cached) {
-//   cached = (global as any).mongoose = {
-//     conn: null,
-//     promise: null,
-//   };
-// }
-
-// export async function databaseConnection() {
-//   if (cached.conn) return cached.conn;
-
-//   if (!MONGODB_URL) {
-//     throw new Error("MONGODB_URL is not defined");
-//   }
-
-//   cached.promise =
-//     cached.promise ||
-//     mongoose.connect(MONGODB_URL, {
-//       dbName: "clerk-next-14-db",
-//       bufferCommands: false,
-//       connectTimeoutMS: 30000,
-//     });
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
