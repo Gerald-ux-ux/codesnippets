e6829391211bfb83f5ff1b4129bbacca4ee4e712
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-05-29 02:48:13 +0300

    fix: adding type safety

--- app/snippets/add/hooks/useUploadSnippet.tsx
+++ app/snippets/add/hooks/useUploadSnippet.tsx
@@ -12,7 +12,7 @@ export default function useUploadSnippet({
   action,
   setOpen,
 }: {
-  selectedSnippet: Snippet;
+  selectedSnippet?: Snippet;
   action?: string;
   setOpen?: (value: boolean) => void;
 }) {
@@ -44,35 +44,35 @@ export default function useUploadSnippet({
     index: number,
     language: { label: string; value: string }
   ) => {
-    const newEditors = [...editor];
+    const newEditors = [...editor!];
     newEditors[index].lang = language;
     setEditor(newEditors);
   };
 
   const handleHeadingChange = (index: number, newHeading: string) => {
-    const newEditors = [...editor];
+    const newEditors = [...editor!];
     newEditors[index].heading = newHeading;
     setEditor(newEditors);
   };
   const handleAdd = (e: any) => {
     e.preventDefault();
-    setEditor([...editor, { heading: "", lang: values[0], code: "" }]);
+    setEditor([...editor!, { heading: "", lang: values[0], code: "" }]);
   };
 
   const handleCodeChange = (index: number, newCode: string) => {
-    const newEditors = [...editor];
+    const newEditors = [...editor!];
     newEditors[index].code = newCode;
     setEditor(newEditors);
   };
 
   const handleDelete = (id: number, e: any) => {
     e.preventDefault();
-    setEditor(editor.filter((_: any, i: any) => i !== id));
+    setEditor(editor!.filter((_: any, i: any) => i !== id));
   };
 
   const handleSubmit = async (formData: FormData) => {
     if (action === "edit") {
-      const id = selectedSnippet._id;
+      const id = selectedSnippet!._id;
       const res = await editCodeSnippet(formData, editor, id);
       if (res.success) {
         setOpen!(false);
